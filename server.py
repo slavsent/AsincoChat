fdbd1c633b747cf1d1ef6168c06f4ed438120cec
sslava <ricar@mail.ru>
2023-04-22 16:36:41 +0300

    Add client t client mesages

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import json
 import datetime
 import sys
 import re
+import select
 import logging
 from log.server_log_config import server_logger
 from decorate import Log
@@ -33,7 +34,8 @@ def create_msg_to_client(msg_from_client):
         }
 
 
-def main():
+@Log()
+def pars_ip_and_port():
     try:
         server_logger.info('Проверка на наличие параметра -p и ввода порта')
         if '-p' in sys.argv:
@@ -76,44 +78,93 @@ def main():
             'Адрес может быть только вида и в диапазоне от 0.0.0.0 до 999.999.999.999 '
             'Поэтому присвоен адрес по умолчанию')
         serv_addr = ''
+    return serv_addr, serv_port
+
+
+@Log()
+def get_message(client):
+    data = client.recv(1000000)
+    server_logger.debug('Получение сообщения от клиента')
+    try:
+        server_logger.info('Попытка расшибровки сообщения от клиента')
+        data_jim = data.decode('utf-8')
+        data_from_client = json.loads(data_jim)
+    except (ValueError, json.JSONDecodeError):
+        server_logger.error('Неудачная расшифровка сообщения!')
+        data_from_client = {
+            "message": 'Error',
+            "time": 'Error'
+        }
+        return data_from_client
+    else:
+        return data_from_client
+
+
+def main():
+    serv_addr, serv_port = pars_ip_and_port()
 
     s = socket(AF_INET, SOCK_STREAM)
     server_logger.info('Создан socket для соединения')
     s.bind((serv_addr, serv_port))
+    s.settimeout(0.4)
+
+    clients = []
+    messages = []
+
     s.listen(5)
     server_logger.info('Прослушивание адреса для соединения')
-    client, addr = s.accept()
-    server_logger.debug('Подключение клиента к серверу')
+
     while True:
-
-        data = client.recv(1000000)
-        server_logger.debug('Получение сообщения от клиента')
         try:
-            server_logger.info('Попытка расшибровки сообщения от клиента')
-            data_jim = data.decode('utf-8')
-            data_from_client = json.loads(data_jim)
-        except (ValueError, json.JSONDecodeError):
-            server_logger.error('Неудачная расшифровка сообщения!')
-            data_jim = {}
-            data_from_client = {
-                "message": 'Error',
-                "time": 'Error'
-            }
+            server_logger.debug('Подключение клиента к серверу')
+            client, addr = s.accept()
+        except OSError:
+            pass
+        else:
+            server_logger.info(f'Установлено соедение с клиентом {addr}')
+            clients.append(client)
 
-        print(data_jim)
+        finally:
+            wait = 10
+            receive_data, send_data, err_data = [], [], []
+            try:
+                if clients:
+                    receive_data, send_data, err_data = select.select(clients, clients, [], wait)
+            except OSError:
+                pass
 
-        print(f'Сообщение от клиента: {data_from_client["message"]}, время: {data_from_client["time"]}')
-        server_logger.info(f'Просмотр сообщения от клиента - {data_from_client["message"]}')
-        data_msg = create_msg_to_client(data_from_client)
-        msg_jim = json.dumps(data_msg)
+            if receive_data:
+                for client_with_message in receive_data:
+                    try:
+                        data_from_client = get_message(client_with_message)
+                        print(f'Сообщение от клиента: {data_from_client["message"]}, время: {data_from_client["time"]}')
+                        server_logger.info(f'Просмотр сообщения от клиента - {data_from_client["message"]}')
+                        if data_from_client["message"] != "Error":
+                            messages.append((data_from_client['user']['account_name'], data_from_client["message"]))
+                        data_msg = create_msg_to_client(data_from_client)
+                        msg_jim = json.dumps(data_msg)
+                        server_logger.debug('Сообщение отправлено клиенту ответ')
+                        client_with_message.send(msg_jim.encode('utf-8'))
+                    except:
+                        server_logger.info(f'Клиент {client_with_message.getpeername()} отключился от сервера.')
+                        clients.remove(client_with_message)
 
-        client.send(msg_jim.encode('utf-8'))
-
-        server_logger.debug('Сообщение отправлено клиенту')
-        if data_from_client['message'] == 'close':
-            server_logger.debug(f'Выход с текущего соединения по сообщению: {data_from_client["message"]}')
-            client.close()
-            return
+            if messages and send_data:
+                message = {
+                    "action": 'message',
+                    "sender": messages[0][0],
+                    'time': datetime.datetime.now().timestamp(),
+                    "message": messages[0][1],
+                }
+                del messages[0]
+                for waiting_client in send_data:
+                    try:
+                        msg_jim = json.dumps(message)
+                        waiting_client.send(msg_jim.encode('utf-8'))
+                        server_logger.debug('Сообщение отправлено клиенту рассылка')
+                    except:
+                        server_logger.info(f'Клиент {waiting_client.getpeername()} отключился от сервера.')
+                        clients.remove(waiting_client)
 
 
 if __name__ == '__main__':
