48d7f50e7ba754b0f372afeedc648f1dea25c884
sslava <ricar@mail.ru>
2023-05-16 13:49:14 +0300

    Add metaclasses

--- client.py
+++ client.py
@@ -8,21 +8,151 @@ import time
 import logging
 from log.client_log_config import client_logger
 from decorate import Log
+from metaclasses import ClientVerifier
 
 
-@Log()
-def create_msg_for_server(msg, username):
-    client_logger.debug(f'Сформировано presence сообщение по сообщению клиента: {msg}')
-    return {
-        'action': 'presence',
-        'time': datetime.datetime.now().timestamp(),
-        'encoding': 'utf-8',
-        'message': msg,
-        'user': {
-            "account_name": username,
-            "status": "I ok!"
+class Client(metaclass=ClientVerifier):
+    def __init__(self, serv_addr, serv_port):
+        self.serv_addr = serv_addr
+        self.serv_port = serv_port
+
+    @Log()
+    def create_msg_for_server(self, msg, username):
+        client_logger.debug(f'Сформировано presence сообщение по сообщению клиента: {msg}')
+        return {
+            'action': 'presence',
+            'time': datetime.datetime.now().timestamp(),
+            'encoding': 'utf-8',
+            'message': msg,
+            'user': {
+                "account_name": username,
+                "status": "I ok!"
+            }
         }
-    }
+
+
+    @Log()
+    def message_from_server(self, s, username):
+        while True:
+            try:
+                data = s.recv(1000000)
+                client_logger.debug('Получено сообщение от сервера')
+                client_logger.debug('Попытка расшифровки сообщения от сервера')
+                data_jim = data.decode('utf-8')
+                data_from_server = json.loads(data_jim)
+                if 'response' in data_from_server:
+                    print(f'Сообщение от сервера: {data_from_server["message"]}, время: {data_from_server["time"]}')
+                elif 'message' in data_from_server and 'user_to' in data_from_server and data_from_server[
+                    'user_to'] == username:
+                    print(
+                        f'Получено сообщение от: {data_from_server["user_from"]}, время: {data_from_server["time"]}\n '
+                        f'Сообщение: {data_from_server["message"]}')
+            except (ValueError, json.JSONDecodeError):
+                client_logger.error('Неудачная расшифровка сообщения!')
+            except (ConnectionResetError, ConnectionError, ConnectionAbortedError):
+                client_logger.error(f'Соединение с сервером было потеряно.')
+                break
+            else:
+                client_logger.debug('Cообщениt от сервера успешно расшифровано')
+
+
+    @Log()
+    def user_command(self, sock, username):
+        print('Для отправки сообщения клиенту введите команду - message, для выхода - exit.')
+
+        while True:
+            command = input('Введите команду: ')
+            if command == 'message':
+                self.create_message(sock, username)
+            elif command == 'exit':
+                msg_jim = json.dumps(self.create_exit_message(username))
+                client_logger.info('Сообщение переведено в формат JSON')
+
+                sock.send(msg_jim.encode('utf-8'))
+                client_logger.debug('Сообщение отправлено на сервер')
+                print('Завершение соединения.')
+                client_logger.info('Завершение работы по команде пользователя.')
+                time.sleep(0.5)
+                break
+            else:
+                print('Введена не верная команда.')
+
+
+    @Log()
+    def create_exit_message(self, account_name):
+        """Функция создаёт словарь с сообщением о выходе"""
+        return {
+            'action': 'exit',
+            'time': datetime.datetime.now().timestamp(),
+            'user': {
+                "account_name": account_name,
+                "status": "I ok!"
+            }
+        }
+
+
+    @Log()
+    def create_message(self, sock, account_name='Guest'):
+        to_user = input('Введите получателя сообщения: ')
+        message = input('Введите сообщение для отправки: ')
+        message_dict = {
+            'action': 'message',
+            'user_to': to_user,
+            'message': message,
+            'time': datetime.datetime.now().timestamp(),
+            'user': {
+                "account_name": account_name,
+                "status": "I ok!"
+            }
+        }
+        client_logger.debug(f'Сформирован словарь сообщения: {message_dict}')
+        try:
+            msg_jim = json.dumps(message_dict)
+            client_logger.info('Сообщение переведено в формат JSON')
+
+            sock.send(msg_jim.encode('utf-8'))
+            client_logger.info(f'Отправлено сообщение для пользователя {to_user}')
+        except:
+            client_logger.critical('Потеряно соединение с сервером.')
+            sys.exit(1)
+
+    def main(self):
+        client_name = input('Введите имя пользователя: ')
+
+        s = socket(AF_INET, SOCK_STREAM)
+        client_logger.info('Создан socket для соединения')
+        # проверка metaclass
+        #s.listen(5)
+        #client, addr = s.accept()
+        try:
+            s.connect((self.serv_addr, self.serv_port))
+        except TimeoutError:
+            client_logger.critical('Сервер не найден')
+            return
+        client_logger.debug('Установлено соединение с сервером')
+
+        msg = 'Установка соединения'
+        data_msg = self.create_msg_for_server(msg, client_name)
+        msg_jim = json.dumps(data_msg)
+        client_logger.info('Сообщение переведено в формат JSON')
+
+        s.send(msg_jim.encode('utf-8'))
+        client_logger.debug('Сообщение отправлено на сервер')
+
+        receiver = threading.Thread(target=self.message_from_server, args=(s, client_name))
+        receiver.daemon = True
+        receiver.start()
+
+        user_interface = threading.Thread(target=self.user_command, args=(s, client_name))
+        user_interface.daemon = True
+        user_interface.start()
+        client_logger.debug('Запущены процессы')
+
+        while True:
+            time.sleep(1)
+            if receiver.is_alive() and user_interface.is_alive():
+                continue
+            break
 
 
 @Log()
@@ -69,128 +199,10 @@ def arg_parser():
     return serv_addr, serv_port
 
 
-@Log()
-def message_from_server(s, username):
-    while True:
-        try:
-            data = s.recv(1000000)
-            client_logger.debug('Получено сообщение от сервера')
-            client_logger.debug('Попытка расшифровки сообщения от сервера')
-            data_jim = data.decode('utf-8')
-            data_from_server = json.loads(data_jim)
-            if 'response' in data_from_server:
-                print(f'Сообщение от сервера: {data_from_server["message"]}, время: {data_from_server["time"]}')
-            elif 'message' in data_from_server and 'user_to' in data_from_server and data_from_server[
-                'user_to'] == username:
-                print(
-                    f'Получено сообщение от: {data_from_server["user_from"]}, время: {data_from_server["time"]}\n '
-                    f'Сообщение: {data_from_server["message"]}')
-        except (ValueError, json.JSONDecodeError):
-            client_logger.error('Неудачная расшифровка сообщения!')
-        except (ConnectionResetError, ConnectionError, ConnectionAbortedError):
-            client_logger.error(f'Соединение с сервером было потеряно.')
-            break
-        else:
-            client_logger.debug('Cообщениt от сервера успешно расшифровано')
-
-
-@Log()
-def user_command(sock, username):
-    print('Для отправки сообщения клиенту введите команду - message, для выхода - exit.')
-
-    while True:
-        command = input('Введите команду: ')
-        if command == 'message':
-            create_message(sock, username)
-        elif command == 'exit':
-            msg_jim = json.dumps(create_exit_message(username))
-            client_logger.info('Сообщение переведено в формат JSON')
-
-            sock.send(msg_jim.encode('utf-8'))
-            client_logger.debug('Сообщение отправлено на сервер')
-            print('Завершение соединения.')
-            client_logger.info('Завершение работы по команде пользователя.')
-            time.sleep(0.5)
-            break
-        else:
-            print('Введена не верная команда.')
-
-
-@Log()
-def create_exit_message(account_name):
-    """Функция создаёт словарь с сообщением о выходе"""
-    return {
-        'action': 'exit',
-        'time': datetime.datetime.now().timestamp(),
-        'user': {
-            "account_name": account_name,
-            "status": "I ok!"
-        }
-    }
-
-
-@Log()
-def create_message(sock, account_name='Guest'):
-    to_user = input('Введите получателя сообщения: ')
-    message = input('Введите сообщение для отправки: ')
-    message_dict = {
-        'action': 'message',
-        'user_to': to_user,
-        'message': message,
-        'time': datetime.datetime.now().timestamp(),
-        'user': {
-            "account_name": account_name,
-            "status": "I ok!"
-        }
-    }
-    client_logger.debug(f'Сформирован словарь сообщения: {message_dict}')
-    try:
-        msg_jim = json.dumps(message_dict)
-        client_logger.info('Сообщение переведено в формат JSON')
-
-        sock.send(msg_jim.encode('utf-8'))
-        client_logger.info(f'Отправлено сообщение для пользователя {to_user}')
-    except:
-        client_logger.critical('Потеряно соединение с сервером.')
-        sys.exit(1)
-
-
 def main():
     serv_addr, serv_port = arg_parser()
-    client_name = input('Введите имя пользователя: ')
-
-    s = socket(AF_INET, SOCK_STREAM)
-    client_logger.info('Создан socket для соединения')
-
-    try:
-        s.connect((serv_addr, serv_port))
-    except TimeoutError:
-        client_logger.critical('Сервер не найден')
-        return
-    client_logger.debug('Установлено соединение с сервером')
-
-    msg = 'Установка соединения'
-    data_msg = create_msg_for_server(msg, client_name)
-    msg_jim = json.dumps(data_msg)
-    client_logger.info('Сообщение переведено в формат JSON')
-
-    s.send(msg_jim.encode('utf-8'))
-    client_logger.debug('Сообщение отправлено на сервер')
-
-    receiver = threading.Thread(target=message_from_server, args=(s, client_name))
-    receiver.daemon = True
-    receiver.start()
-
-    user_interface = threading.Thread(target=user_command, args=(s, client_name))
-    user_interface.daemon = True
-    user_interface.start()
-    client_logger.debug('Запущены процессы')
-
-    while True:
-        time.sleep(1)
-        if receiver.is_alive() and user_interface.is_alive():
-            continue
-        break
+    client_new = Client(serv_addr, serv_port)
+    client_new.main()
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -7,60 +7,156 @@ import select
 import logging
 from log.server_log_config import server_logger
 from decorate import Log
+from port_descriptor import PortDescriptor
+from metaclasses import ServerVerifier
 
 
-@Log()
-def create_msg_to_client(msg_from_client, username, clients, client):
-    if msg_from_client['message'] == 'Error':
-        server_logger.debug('Сформирован ответ с - Bad Request')
-        return {
-            "response": 400,
-            "message": "Bad Request",
-            'time': datetime.datetime.now().timestamp(),
-        }
-    if 'user' in msg_from_client and 'action' in msg_from_client and msg_from_client['action'] == 'presence':
-        if msg_from_client['user']['account_name'] in username.keys():
+class Server(metaclass=ServerVerifier):
+    serv_port = PortDescriptor()
+
+    def __init__(self, serv_addr, serv_port):
+        self.serv_addr = serv_addr
+        self.serv_port = serv_port
+
+    @Log()
+    def create_msg_to_client(self, msg_from_client, username, clients, client):
+        if msg_from_client['message'] == 'Error':
+            server_logger.debug('Сформирован ответ с - Bad Request')
             return {
                 "response": 400,
-                'user': 'error',
-                "message": f"Привет клиент: {msg_from_client['user']['account_name']} такой username уже существует",
+                "message": "Bad Request",
                 'time': datetime.datetime.now().timestamp(),
             }
-        else:
-            username[msg_from_client['user']['account_name']] = client
-    if msg_from_client['action'] == 'presence':
-        server_logger.debug('Сформирован ответ на сообщение - presence')
-        return {
-            "response": 200,
-            "message": f"Привет клиент: {msg_from_client['user']['account_name']}",
-            'time': datetime.datetime.now().timestamp(),
-        }
-    elif msg_from_client['action'] == 'message' and 'user_to' in msg_from_client and 'user' in msg_from_client:
-        if msg_from_client['user_to'] in username.keys():
+        if 'user' in msg_from_client and 'action' in msg_from_client and msg_from_client['action'] == 'presence':
+            if msg_from_client['user']['account_name'] in username.keys():
+                return {
+                    "response": 400,
+                    'user': 'error',
+                    "message": f"Привет клиент: {msg_from_client['user']['account_name']} такой username уже существует",
+                    'time': datetime.datetime.now().timestamp(),
+                }
+            else:
+                username[msg_from_client['user']['account_name']] = client
+        if msg_from_client['action'] == 'presence':
+            server_logger.debug('Сформирован ответ на сообщение - presence')
             return {
-                "action": 'message',
-                'user_from': msg_from_client['user']['account_name'],
-                'user_to': msg_from_client['user_to'],
-                "message": msg_from_client['message'],
+                "response": 200,
+                "message": f"Привет клиент: {msg_from_client['user']['account_name']}",
                 'time': datetime.datetime.now().timestamp(),
             }
-        return {
-            "response": 400,
-            "message": f"Привет клиент: {msg_from_client['user']['account_name']}. Такого получателя нет",
-            'time': datetime.datetime.now().timestamp(),
-        }
-    elif 'action' in msg_from_client and msg_from_client['action'] == 'exit' and 'user' in msg_from_client:
-        clients.remove(username[msg_from_client['user']['account_name']])
-        username[msg_from_client['user']['account_name']].close()
-        del username[msg_from_client['user']['account_name']]
-        return {'response': 'close'}
-    else:
-        server_logger.debug('Сформирован ответ на прочие сообщения чем presence и error')
-        return {
-            "response": 200,
-            "message": "Wait action",
-            'time': datetime.datetime.now().timestamp(),
-        }
+        elif msg_from_client['action'] == 'message' and 'user_to' in msg_from_client and 'user' in msg_from_client:
+            if msg_from_client['user_to'] in username.keys():
+                return {
+                    "action": 'message',
+                    'user_from': msg_from_client['user']['account_name'],
+                    'user_to': msg_from_client['user_to'],
+                    "message": msg_from_client['message'],
+                    'time': datetime.datetime.now().timestamp(),
+                }
+            return {
+                "response": 400,
+                "message": f"Привет клиент: {msg_from_client['user']['account_name']}. Такого получателя нет",
+                'time': datetime.datetime.now().timestamp(),
+            }
+        elif 'action' in msg_from_client and msg_from_client['action'] == 'exit' and 'user' in msg_from_client:
+            clients.remove(username[msg_from_client['user']['account_name']])
+            username[msg_from_client['user']['account_name']].close()
+            del username[msg_from_client['user']['account_name']]
+            return {'response': 'close'}
+        else:
+            server_logger.debug('Сформирован ответ на прочие сообщения чем presence и error')
+            return {
+                "response": 200,
+                "message": "Wait action",
+                'time': datetime.datetime.now().timestamp(),
+            }
+
+    @Log()
+    def get_message(self, client):
+        data = client.recv(1000000)
+        server_logger.debug('Получение сообщения от клиента')
+        try:
+            server_logger.info('Попытка расшибровки сообщения от клиента')
+            data_jim = data.decode('utf-8')
+            data_from_client = json.loads(data_jim)
+        except (ValueError, json.JSONDecodeError):
+            server_logger.error('Неудачная расшифровка сообщения!')
+            data_from_client = {
+                "message": 'Error',
+                "time": 'Error'
+            }
+        return data_from_client
+
+    def main(self):
+
+        s = socket(AF_INET, SOCK_STREAM)
+        # проверка работы метакласса
+        # s = socket()
+        server_logger.info('Создан socket для соединения')
+        s.bind((self.serv_addr, self.serv_port))
+        # проверка работы метакласса
+        # s.connect((self.serv_addr, self.serv_port))
+        s.settimeout(0.4)
+
+        clients = []
+        messages = []
+
+        username = {}
+
+        s.listen(5)
+        server_logger.info('Прослушивание адреса для соединения')
+
+        while True:
+            try:
+                server_logger.debug('Подключение клиента к серверу')
+                client, addr = s.accept()
+            except OSError:
+                pass
+            else:
+                server_logger.info(f'Установлено соедение с клиентом {addr}')
+                clients.append(client)
+
+            wait = 10
+            receive_data, send_data, err_data = [], [], []
+            try:
+                if clients:
+                    receive_data, send_data, err_data = select.select(clients, clients, [], wait)
+            except OSError:
+                pass
+
+            if receive_data:
+                for client_with_message in receive_data:
+                    try:
+                        data_from_client = self.get_message(client_with_message)
+                        print(f'Сообщение от клиента: {data_from_client["message"]}, время: {data_from_client["time"]}')
+                        server_logger.info(f'Просмотр сообщения от клиента - {data_from_client["message"]}')
+                        data_msg = self.create_msg_to_client(data_from_client, username, clients, client_with_message)
+                        if 'response' not in data_msg or data_msg['response'] != 'close':
+                            if 'action' in data_msg and data_msg['action'] == 'message':
+                                messages.append(data_msg)
+                            else:
+                                msg_jim = json.dumps(data_msg)
+                                server_logger.debug('Сообщение отправлено клиенту ответ')
+                                client_with_message.send(msg_jim.encode('utf-8'))
+                                if 'user' in data_msg and data_msg['user'] == 'error':
+                                    clients.remove(client_with_message)
+                    except Exception:
+                        server_logger.info(f'Клиент {client_with_message.getpeername()} отключился от сервера.')
+                        clients.remove(client_with_message)
+
+            if messages:
+                for message in messages:
+                    try:
+                        msg_jim = json.dumps(message)
+                        server_logger.debug(
+                            f'Сообщение отправлено клиенту {message["user_to"]} от клиента {message["user_from"]}')
+                        username[message['user_to']].send(msg_jim.encode('utf-8'))
+                    except:
+                        server_logger.info(
+                            f'Клиент {username[message["user_to"]].getpeername()} отключился от сервера.')
+                        clients.remove(username[message['user_to']])
+                        del username[message['user_to']]
+                messages.clear()
 
 
 @Log()
@@ -110,89 +206,12 @@ def pars_ip_and_port():
     return serv_addr, serv_port
 
 
-@Log()
-def get_message(client):
-    data = client.recv(1000000)
-    server_logger.debug('Получение сообщения от клиента')
-    try:
-        server_logger.info('Попытка расшибровки сообщения от клиента')
-        data_jim = data.decode('utf-8')
-        data_from_client = json.loads(data_jim)
-    except (ValueError, json.JSONDecodeError):
-        server_logger.error('Неудачная расшифровка сообщения!')
-        data_from_client = {
-            "message": 'Error',
-            "time": 'Error'
-        }
-    return data_from_client
-
-
 def main():
     serv_addr, serv_port = pars_ip_and_port()
-
-    s = socket(AF_INET, SOCK_STREAM)
-    server_logger.info('Создан socket для соединения')
-    s.bind((serv_addr, serv_port))
-    s.settimeout(0.4)
-
-    clients = []
-    messages = []
-
-    username = {}
-
-    s.listen(5)
-    server_logger.info('Прослушивание адреса для соединения')
-
-    while True:
-        try:
-            server_logger.debug('Подключение клиента к серверу')
-            client, addr = s.accept()
-        except OSError:
-            pass
-        else:
-            server_logger.info(f'Установлено соедение с клиентом {addr}')
-            clients.append(client)
-
-        wait = 10
-        receive_data, send_data, err_data = [], [], []
-        try:
-            if clients:
-                receive_data, send_data, err_data = select.select(clients, clients, [], wait)
-        except OSError:
-            pass
-
-        if receive_data:
-            for client_with_message in receive_data:
-                try:
-                    data_from_client = get_message(client_with_message)
-                    print(f'Сообщение от клиента: {data_from_client["message"]}, время: {data_from_client["time"]}')
-                    server_logger.info(f'Просмотр сообщения от клиента - {data_from_client["message"]}')
-                    data_msg = create_msg_to_client(data_from_client, username, clients, client_with_message)
-                    if 'response' not in data_msg or data_msg['response'] != 'close':
-                        if 'action' in data_msg and data_msg['action'] == 'message':
-                            messages.append(data_msg)
-                        else:
-                            msg_jim = json.dumps(data_msg)
-                            server_logger.debug('Сообщение отправлено клиенту ответ')
-                            client_with_message.send(msg_jim.encode('utf-8'))
-                            if 'user' in data_msg and data_msg['user'] == 'error':
-                                clients.remove(client_with_message)
-                except Exception:
-                    server_logger.info(f'Клиент {client_with_message.getpeername()} отключился от сервера.')
-                    clients.remove(client_with_message)
-
-        if messages:
-            for message in messages:
-                try:
-                    msg_jim = json.dumps(message)
-                    server_logger.debug(
-                        f'Сообщение отправлено клиенту {message["user_to"]} от клиента {message["user_from"]}')
-                    username[message['user_to']].send(msg_jim.encode('utf-8'))
-                except:
-                    server_logger.info(f'Клиент {username[message["user_to"]].getpeername()} отключился от сервера.')
-                    clients.remove(username[message['user_to']])
-                    del username[message['user_to']]
-            messages.clear()
+    # проверка работы класса на правильный порт
+    # new_serv = Server(serv_addr, 123)
+    new_serv = Server(serv_addr, serv_port)
+    new_serv.main()
 
 
 if __name__ == '__main__':
